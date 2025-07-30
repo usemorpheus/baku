dfd34f22a23d7b559c80164a99da72c214443b43
Alex <alex@parse.cn>
2025-07-30 12:49:16 +0800

    wip

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,3 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+\Illuminate\Support\Facades\Schedule::command('baku:pull_twitter_mentions')->everyFiveMinutes();
