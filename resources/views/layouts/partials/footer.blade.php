3f5c90c837778257540ee2ed329102942163b09b
Alex <alex@parse.cn>
2025-08-15 20:28:48 +0800

    Update footer.blade.php content

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -6,7 +6,7 @@
                 <img src="{{asset('images/baku/logo-white.png')}}" style="height: 40px;"/>
                 <div class="text-white" style="color: #F6F4F3;">
                     <h3 class="m-0 fs-5">Baku Agent</h3>
-                    <p>A shadow reporter for everyone</p>
+                    <p>Your voice, your reporter</p>
                 </div>
             </div>
         </div>
