fd345af968d199285e9a566c8e00a27e66bebe8e
Alex <alex@parse.cn>
2025-08-19 17:16:47 +0800

    Remove unnecessary variable and update view compact parameters

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,6 @@ class HomeController
 {
     public function __invoke()
     {
-        $webchat_link      = "https://play.baku.builders/chat/share?shareId=pJaog9I66rW1x2L8JTkHZFjL";
         $twitter_link      = "https://x.com/Baku_agent";
         $telegram_bot_link = "https://t.me/baku_news_bot";
         $news_link         = route('articles.index');
@@ -23,6 +22,6 @@ class HomeController
             ];
         }
 
-        return view('home', compact('webchat_link', 'twitter_link', 'telegram_bot_link', 'news_link', 'chat_links'));
+        return view('home', compact('twitter_link', 'telegram_bot_link', 'news_link', 'chat_links'));
     }
 }
