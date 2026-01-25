0c2723e01f2a6784bb2fc068cd72010ef9b23bbb
Babbage <qingpo.gao@gmail.com>
2026-01-26 00:49:20 +0800

    fix: remove date from search option

--- app/Http/Controllers/Webhooks/TelegramController.php
+++ app/Http/Controllers/Webhooks/TelegramController.php
@@ -232,11 +232,11 @@ class TelegramController
             [
                 // 唯一标识条件
                 'telegram_chat_id' => $metrics['telegram_chat_id'],
-                'date' => $metrics['date'],
                 'dimension' => $metrics['dimension'],
             ],
             [
                 // 要更新/创建的字段
+                'date' => $metrics['date'],
                 'year' => $metrics['year'],
                 'month' => $metrics['month'],
                 'day' => $metrics['day'],
