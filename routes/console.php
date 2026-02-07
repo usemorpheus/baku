6e097820ae0dac08d970aa938f21a57d07ac626e
Babbage <qingpo.gao@gmail.com>
2026-02-08 00:40:40 +0800

    fix: delete console Schedule

--- routes/console.php
+++ routes/console.php
@@ -5,11 +5,11 @@ use App\Actions\UpdateBakuCommunity;
 use App\Actions\CalculateRankings;
 
 // 每日更新Baku社区数据
-Schedule::call(function () {
-    // UpdateBakuCommunity::run();
-})->dailyAt('00:00');
+// Schedule::call(function () {
+//     // UpdateBakuCommunity::run();
+// })->dailyAt('00:00');
 
 // 每日计算排名
-Schedule::call(function () {
-    CalculateRankings::run();
-})->dailyAt('00:05'); // 在更新数据后5分钟计算排名
+// Schedule::call(function () {
+//     CalculateRankings::run();
+// })->dailyAt('00:05'); // 在更新数据后5分钟计算排名
